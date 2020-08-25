e8f6f2ecabd2556696e1d0020e9a3892e751807b
Dousheng <1941408616@qq.com>
2020-08-25 18:55:18 +0800

    Ribbon 重试

--- sp06-ribbon/src/main/java/cn/tedu/sp06/Sp06RibbonApplication.java
+++ sp06-ribbon/src/main/java/cn/tedu/sp06/Sp06RibbonApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -16,6 +17,10 @@ public class Sp06RibbonApplication {
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(1000);
+        factory.setReadTimeout(1000);
+
+        return new RestTemplate(factory);
     }
 }
