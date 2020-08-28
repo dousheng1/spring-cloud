921197e4e9557c31b586ea61db41b1be8446b125
Dousheng <1941408616@qq.com>
2020-08-28 18:58:39 +0800

    feign 测试

---
+++ sp08-hystrix-dashboard/src/main/java/cn/tedu/sp08/Sp08HystrixDashboardApplication.java
@@ -0,0 +1,27 @@
+package cn.tedu.sp08;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+@EnableHystrixDashboard
+@SpringBootApplication
+public class Sp08HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp08HystrixDashboardApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
